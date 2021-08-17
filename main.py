cc095965c6a0af7005fb7744576c5e72332f2f3c
tsherknus <tsherknus@mail.usf.edu>
2021-08-16 21:02:50 -0400

    S&P 500 Data Aggregation Software - Gathers high, low, open, close, and volume values in 5 minute intervals for 505 S&P stock tickers. - Values are stored in a local mySQL database to facilitate low overhead data analysis via Python linear regression algorithms.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import time
-
 import pandas as pd
 import pandas_datareader as dr
 import matplotlib as plt
@@ -7,27 +6,13 @@ import yfinance as yf
 import pymysql
 from sqlalchemy import create_engine
 
-# retrieve historical data with pandas_datareader
-# pandas_datareader
-# tickers = ['msft', 'aapl', 'twtr', 'intc']
-#
-# df1 = dr.DataReader(tickers, data_source='yahoo')
-#
-# df = dr.data.get_data_yahoo('IBM', start='2021-06-24', end='2021-07-26')
-#
-# df.info()
-#
-# df['Close'].plot(figsize=(15,5))
-#
-# plt.pyplot.show()
-
 s_and_p_500_config = open('S_and_p_500_tickers_config.txt', 'r')
 
 s_and_p_500 = s_and_p_500_config.readlines()
 
 engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
-                       .format(user="root",
-                               pw="******",
+                       .format(user="",
+                               pw="",
                                db="s_and_p_500"))
 
 for ticker in s_and_p_500:

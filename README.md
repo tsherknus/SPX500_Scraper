8dcba5572a159e6948576d48afc878aa8c75de25
tsherknus <tsherknus@mail.usf.edu>
2021-08-26 08:46:30 -0400

    S&P 500 Data Aggregation Software - Gathers high, low, open, close, and volume values in 5 minute intervals for all tickers listed in the S&P. - Values are stored in a local mySQL database to facilitate low overhead data analysis via Python linear regression algorithms.
    - Gathers high, low, open, close, and volume values in 1 day intervals for all tickers listed in the S&P.
    - Values are stored in a local mySQL database to facilitate low overhead data analysis via Python linear regression algorithms.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 S&P 500 Data Aggregation Software
 - Gathers high, low, open, close, and volume values in 5 minute intervals for 505 S&P stock tickers. 
 - Values are stored in a local mySQL database to facilitate low overhead data analysis via Python linear regression algorithms.
+    
+
+- Gathers high, low, open, close, and volume values in 1 day intervals for all tickers listed in the S&P.
+- Values are stored in a local mySQL database to facilitate low overhead data analysis via Python linear regression algorithms.
+
+On the horizon:
+- Record earnings dates up to 180 days in advance. Primarily utilization of earnings dates as changepoints in a stocks profitability.
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ s_and_p_500 = s_and_p_500_config.readlines()
 engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
                        .format(user="",
                                pw="",
-                               db="s_and_p_500"))
+                               db=""))
 
 for ticker in s_and_p_500:
     print(ticker.strip().lower())
@@ -28,10 +28,22 @@ for ticker in s_and_p_500:
         time.sleep(1.1)
 
     except Exception as e:
-        print(ticker.strip() + " failed.")
+        print(ticker.strip() + " 5m failed.")
 
     # Underlying Daily Total Volume
     # Populates table: undl_daily_total_vol
+    try:
+        data = yf.download(tickers=ticker.strip(), period='1d', interval='1d')
+
+        tableName = ticker.strip().lower() + '_daily'
+
+        data.to_sql(tableName, con=engine, if_exists='append', chunksize=1000)
+
+        time.sleep(1.1)
+
+    except Exception as e:
+        print(ticker.strip() + " 1d failed.")
+
 
     # Underlying Last Earnings Date
     # Populates table: undl_last_earnings

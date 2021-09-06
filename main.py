e6de12b793290ef7d2193e6063454bc4ad439389
tsherknus <tsherknus@mail.usf.edu>
2021-09-06 18:18:04 -0400

    S&P 500 Data Aggregation Software - Gathers high, low, open, close, and volume values in 5 minute intervals for all tickers listed in the S&P. - Values are stored in a local mySQL database to facilitate low overhead data analysis via Python linear regression algorithms.
    - Gathers high, low, open, close, and volume values in 1 day intervals for all tickers listed in the S&P.
    - Values are stored in a local mySQL database to facilitate low overhead data analysis via Python linear regression algorithms.
    
    - Gathers past and future earnings date, earnings call time, earnings per share estimated, reported earnings per share, and suprise percentage for all tickers within the S&P 500.

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import matplotlib as plt
 import yfinance as yf
 import pymysql
 from sqlalchemy import create_engine
+from datetime import datetime
+from datetime import timedelta
+from yahoo_earnings_calendar import YahooEarningsCalendar
+import dateutil.parser
+
+import requests
+import json
 
 s_and_p_500_config = open('S_and_p_500_tickers_config.txt', 'r')
 
@@ -15,17 +22,21 @@ engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
                                pw="",
                                db=""))
 
+daysToCollect = 3
+daysToCollectString = str(daysToCollect)+"d"
+
+
 for ticker in s_and_p_500:
     print(ticker.strip().lower())
 
     # Underlying Open, Close, High, Low, Volume (5 Minute Intervals).
     # Populates tables: aapl, aal, amzn, etc.
     try:
-        data = yf.download(tickers=ticker.strip(), period='1d', interval='5m')
+        data = yf.download(tickers=ticker.strip(), period=daysToCollectString, interval='5m')
 
         data.to_sql(ticker.strip().lower(), con=engine, if_exists='append', chunksize=1000)
 
-        time.sleep(1.1)
+        time.sleep(daysToCollect)
 
     except Exception as e:
         print(ticker.strip() + " 5m failed.")
@@ -33,17 +44,51 @@ for ticker in s_and_p_500:
     # Underlying Daily Total Volume
     # Populates table: undl_daily_total_vol
     try:
-        data = yf.download(tickers=ticker.strip(), period='1d', interval='1d')
+        data = yf.download(tickers=ticker.strip(), period=daysToCollectString, interval='1d')
 
         tableName = ticker.strip().lower() + '_daily'
 
         data.to_sql(tableName, con=engine, if_exists='append', chunksize=1000)
 
-        time.sleep(1.1)
+        time.sleep(daysToCollect)
 
     except Exception as e:
         print(ticker.strip() + " 1d failed.")
 
-
     # Underlying Last Earnings Date
     # Populates table: undl_last_earnings
+    # try:
+    #     url = 'https://finance.yahoo.com/calendar/earnings?symbol=' + ticker.strip()
+    #     time.sleep(10)
+    #     page = requests.get(url, headers={'User-Agent': 'Custom'})
+    #     page_content = page.content.decode(encoding='utf-8', errors='strict')
+    #     page_data_string = [row for row in page_content.split(
+    #         '\n') if row.startswith('root.App.main = ')][0][:-1]
+    #     page_data_string = page_data_string.split('root.App.main = ', 1)[1]
+    #
+    #     page_data_dict = json.loads(page_data_string)
+    #
+    #     page_data_dict = page_data_dict["context"]["dispatcher"]["stores"]["ScreenerResultsStore"]["results"]["rows"]
+    #
+    #     output = pd.DataFrame.from_dict(page_data_dict)
+    #
+    #     tableName = ticker.strip().lower() + '_earnings'
+    #
+    #     output.to_sql(tableName, con=engine, if_exists='append', chunksize=1000)
+    #
+    # except Exception as e:
+    #     print(ticker.strip() + " future earnings failed.")
+
+
+    # Get earnings over a set period https://towardsdatascience.com/how-to-download-the-public-companies-earnings-calendar-with-python-9241dd3d15d
+    # DAYS_AHEAD = 180
+    #
+    # # setting the dates
+    # start_date = datetime.now().date()
+    # end_date = (datetime.now().date() + timedelta(days=DAYS_AHEAD))
+    # earnings_df = output ^^^^^^^
+    #
+    # # extracting the date from the string and filtering for the period of interest
+    # earnings_df['report_date'] = earnings_df['startdatetime'].apply(lambda x: dateutil.parser.isoparse(x).date())
+    # earnings_df = earnings_df.loc[earnings_df['report_date'].between(start_date, end_date)] \
+    #                          .sort_values('report_date')
